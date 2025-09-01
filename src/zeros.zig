d6e1792c7fae40ceb7473b7c4c605af122f693e9
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2025-09-01 14:04:54 +0200

    add a hash parameter for zero computation

--- src/zeros.zig
+++ src/zeros.zig
@@ -1,23 +1,29 @@
 // List of root hashes of zero-subtrees, up to depth 255.
 const std = @import("std");
 
-pub const hashes_of_zero: [256][32]u8 = calc: {
+/// Generic function to build zero hashes for any hash function
+/// HashType should be a hash type like std.crypto.hash.sha2.Sha256
+/// digest_length is the output size of the hash in bytes
+pub fn buildZeroHashes(comptime HashType: type, comptime digest_length: usize, comptime depth: usize) [depth][digest_length]u8 {
     @setEvalBranchQuota(10000000);
-    var ret: [256][32]u8 = undefined;
-    
-    var current = [_]u8{0} ** 32;
-    
+    var ret: [depth][digest_length]u8 = undefined;
+
+    var current = [_]u8{0} ** digest_length;
+
     ret[0] = current;
-    
+
     var i: usize = 1;
-    while (i < 256) : (i += 1) {
+    while (i < depth) : (i += 1) {
         // Hash the current level twice (left and right child are the same)
-        var hasher = std.crypto.hash.sha2.Sha256.init(.{});
+        var hasher = HashType.init(.{});
         hasher.update(&current);
         hasher.update(&current);
         current = hasher.finalResult();
         ret[i] = current;
     }
-    
-    break :calc ret;
-};
+
+    return ret;
+}
+
+// SHA256 zero hashes (the default for SSZ)
+pub const hashes_of_zero = buildZeroHashes(std.crypto.hash.sha2.Sha256, 32, 256);
